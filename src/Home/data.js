b614fd1c547a86a4f85963d8e2c82ac82dc4fdfa
jljsj <liangjun.jiang@alipay.com>
2018-01-18 17:14:28 +0800

    add img load

--- src/Home/data.js
+++ src/Home/data.js
@@ -177,3 +177,22 @@ export const footer = [
     ],
   },
 ];
+
+// 图处预加载；
+if (typeof document !== 'undefined') {
+  const div = document.createElement('div');
+  div.style.display = 'none';
+  document.body.appendChild(div);
+  [
+    'https://gw.alipayobjects.com/zos/rmsportal/KtRzkMmxBuWCVjPbBgRY.svg',
+    'https://gw.alipayobjects.com/zos/rmsportal/qIcZMXoztWjrnxzCNTHv.svg',
+    'https://gw.alipayobjects.com/zos/rmsportal/eLtHtrKjXfabZfRchvVT.svg',
+    'https://gw.alipayobjects.com/zos/rmsportal/iVOzVyhyQkQDhRsuyBXC.svg',
+    'https://gw.alipayobjects.com/zos/rmsportal/HxEfljPlykWElfhidpxR.svg',
+    'https://gw.alipayobjects.com/zos/rmsportal/wdarlDDcdCaVoCprCRwB.svg',
+  ].concat(page4.children).forEach((src) => {
+    const img = new Image();
+    img.src = src;
+    div.appendChild(img);
+  });
+}
